853c8529efc6564e315b600193f31a0961eb742d
matteo1110001000100110101 <104920615+matteo1110001000100110101@users.noreply.github.com>
2022-05-13 12:48:48 -0400

    Update Q1Volume.py

--- Q1Volume.py
+++ Q1Volume.py
@@ -1,1 +1,36 @@
-dghg
+# NAME OF AUTHOR:  Matteo Sorgini
+# NAME OF THE PROGRAM: 
+print("Printing the volume of a rectangular prism")
+# DATE OF CREATION:  05-12-2022
+# PURPOSE OF PROGRAM:  
+print("How to calculate the volume of a rectangular prism")
+
+# VARIABLE DEFINITION
+
+length = 0
+width = 0
+height = 0
+Volume = 0
+
+# INPUT
+
+
+length = int(input("Enter length"))
+
+width = int(input("Enter width"))
+
+height = int(input("Enter height"))
+
+
+
+
+# PROCESSING
+
+print("Start")
+print("Your presented volume is")
+print(length*width*height)
+
+# OUTPUT
+
+print("Thank you for calculating the volume of a rectangular prism, have a nice day!")
+
